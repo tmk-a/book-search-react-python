0acf4ffe42bd00c1da57107356663b5844c31c81
Tamaki <avenir.23belle@gmail.com>
2025-05-11 22:42:20 -0700

    feat: implement BookCard component with styling

--- frontend/src/feature/BookCard.tsx
+++ frontend/src/feature/BookCard.tsx
@@ -1,17 +1,45 @@
-// src/feature/BookCard.tsx
-
 // import React from "react";
 
-const BookCard = ({ book }: { book: any }) => {
+import "./BookCard.css";
+
+type book = {
+  id: string;
+  title: string;
+  authors: string;
+  description: string;
+  published_date: string;
+  thumbnail: string;
+  preview_link: string;
+};
+
+const BookCard = ({ book }: { book: book }) => {
+  let outline = book.description;
+  if (outline.length > 100) {
+    outline = outline.substr(0, 200);
+    outline += "...";
+  }
+
   return (
-    <div>
-      <h3>{book.title}</h3>
-      <p>{book.authors?.join(", ")}</p>
-      <img
-        src={book.thumbnail}
-        alt={book.title}
-        style={{ maxWidth: "100px" }}
-      />
+    <div className="bookCard">
+      <div className="bookCard-img">
+        <img
+          src={book.thumbnail}
+          alt={book.title}
+          style={{ maxWidth: "100px" }}
+        />
+      </div>
+      <div className="bookCard-header">
+        <h3>{book.title}</h3>
+      </div>
+      <div className="bookCard-body">
+        <p>{outline}</p>
+      </div>
+      <div className="bookCard-footer">
+        <div className="bookCard-footer-item">
+          <strong>{book.authors}</strong>
+          <span className="muted">{book.published_date}</span>
+        </div>
+      </div>
     </div>
   );
 };
