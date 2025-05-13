9fc8cc99e679aaf974a1cede2ff67bb547269de2
Tamaki <avenir.23belle@gmail.com>
2025-05-13 15:33:57 -0700

    style: remove unnecessary comments and import statement in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,4 @@
-// src/App.tsx
-
-// import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
 import DetailPage from "./pages/DetailPage"; // Create later
 
