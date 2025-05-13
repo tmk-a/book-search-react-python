5e6472892efeed48b16d6331dafbd3a9384c3d76
Tamaki <avenir.23belle@gmail.com>
2025-05-13 16:55:50 -0700

    style: enhance BookCard component with hover effects and improved structure

--- frontend/src/feature/BookCard.tsx
+++ frontend/src/feature/BookCard.tsx
@@ -1,6 +1,5 @@
-// import React from "react";
-
 import "./BookCard.scss";
+import { useState } from "react";
 
 type book = {
   id: string;
@@ -19,28 +18,64 @@ const BookCard = ({ book }: { book: book }) => {
     outline += "...";
   }
 
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
-    <div className="bookCard">
-      <div className="bookCard-img">
-        <img
-          src={book.thumbnail}
-          alt={book.title}
-          style={{ maxWidth: "100%" }}
-        />
-      </div>
-      <div className="bookCard-header">
-        <h3>{book.title}</h3>
-      </div>
-      <div className="bookCard-body">
-        <p>{outline}</p>
-      </div>
-      <div className="bookCard-footer">
-        <div className="bookCard-footer-item">
-          <strong>{book.authors}</strong>
-          <span className="muted">{book.published_date}</span>
+    <>
+      <div
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+      >
+        <div className="bookCard">
+          <img
+            src={book.thumbnail ?? null}
+            alt={book.title}
+            className="bookCard__image"
+            style={{ opacity: isHovering ? "0" : "1" }}
+          />
+          <div
+            className="bookCard bookCard__info"
+            style={{
+              opacity: isHovering ? "1" : "0",
+            }}
+          >
+            <div className="bookCard-header">
+              <h3>{book.title}</h3>
+            </div>
+            <div className="bookCard-body">
+              <p>{outline}</p>
+            </div>
+            <div className="bookCard-footer">
+              <div className="bookCard-footer-item">
+                <strong>{book.authors}</strong>
+                <span className="muted">{book.published_date}</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
+    // <div className="bookCard">
+    //   <div className="bookCard-img">
+    //     <img
+    //       src={book.thumbnail}
+    //       alt={book.title}
+    //       style={{ maxWidth: "100%" }}
+    //     />
+    //   </div>
+    //   <div className="bookCard-header">
+    //     <h3>{book.title}</h3>
+    //   </div>
+    //   <div className="bookCard-body">
+    //     <p>{outline}</p>
+    //   </div>
+    //   <div className="bookCard-footer">
+    //     <div className="bookCard-footer-item">
+    //       <strong>{book.authors}</strong>
+    //       <span className="muted">{book.published_date}</span>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
