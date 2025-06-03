a3db232e1110fbf49f7dfda9c4ef8ccbe1d45a23
“tmk-a” <tamaki.akita.jp@gmail.com>
2025-06-02 23:42:23 -0700

    feat: add link to detail page

--- frontend/src/feature/BookCard.tsx
+++ frontend/src/feature/BookCard.tsx
@@ -1,5 +1,6 @@
 import "./BookCard.scss";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 type book = {
   id: string;
@@ -22,61 +23,42 @@ const BookCard = ({ book }: { book: book }) => {
 
   return (
     <>
-      <div
-        onMouseEnter={() => setIsHovering(true)}
-        onMouseLeave={() => setIsHovering(false)}
-        style={{ paddingTop: "1rem", paddingBottom: "1rem" }}
-      >
-        <div className="bookCard">
-          <img
-            src={book.thumbnail ?? null}
-            alt={book.title}
-            className="bookCard__image"
-            style={{ opacity: isHovering ? "0" : "1" }}
-          />
-          <div
-            className="bookCard bookCard__info"
-            style={{
-              opacity: isHovering ? "1" : "0",
-            }}
-          >
-            <div className="bookCard-header">
-              <h3>{book.title}</h3>
-            </div>
-            <div className="bookCard-body">
-              <p>{outline}</p>
-            </div>
-            <div className="bookCard-footer">
-              <div className="bookCard-footer-item">
-                <strong>{book.authors}</strong>
-                <span className="muted">{book.published_date}</span>
+      <Link to={`/book/${book.id}`}>
+        <div
+          onMouseEnter={() => setIsHovering(true)}
+          onMouseLeave={() => setIsHovering(false)}
+          style={{ paddingTop: "1rem", paddingBottom: "1rem" }}
+        >
+          <div className="bookCard">
+            <img
+              src={book.thumbnail ?? null}
+              alt={book.title}
+              className="bookCard__image"
+              style={{ opacity: isHovering ? "0" : "1" }}
+            />
+            <div
+              className="bookCard bookCard__info"
+              style={{
+                opacity: isHovering ? "1" : "0",
+              }}
+            >
+              <div className="bookCard-header">
+                <h3>{book.title}</h3>
+              </div>
+              <div className="bookCard-body">
+                <p>{outline}</p>
+              </div>
+              <div className="bookCard-footer">
+                <div className="bookCard-footer-item">
+                  <strong>{book.authors}</strong>
+                  <span className="muted">{book.published_date}</span>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Link>
     </>
-    // <div className="bookCard">
-    //   <div className="bookCard-img">
-    //     <img
-    //       src={book.thumbnail}
-    //       alt={book.title}
-    //       style={{ maxWidth: "100%" }}
-    //     />
-    //   </div>
-    //   <div className="bookCard-header">
-    //     <h3>{book.title}</h3>
-    //   </div>
-    //   <div className="bookCard-body">
-    //     <p>{outline}</p>
-    //   </div>
-    //   <div className="bookCard-footer">
-    //     <div className="bookCard-footer-item">
-    //       <strong>{book.authors}</strong>
-    //       <span className="muted">{book.published_date}</span>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
