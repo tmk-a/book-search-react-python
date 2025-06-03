2ab2d3758ef45a007a4576d9a9e31367c563c249
“tmk-a” <tamaki.akita.jp@gmail.com>
2025-06-02 23:45:39 -0700

    feat: implement some search conditions

--- frontend/src/service/api.ts
+++ frontend/src/service/api.ts
@@ -1,8 +1,13 @@
+import { SearchInputT } from "../util/typeUtil";
 const API_BASE_URL = "https://book-search-react-python.onrender.com";
 
-export const fetchBooks = async (query: string, pageNum = 1, pageSize = 20) => {
+export const fetchBooks = async (
+  { title, author, publisher, subject, keyword }: SearchInputT,
+  pageNum = 1,
+  pageSize = 20
+) => {
   const res = await fetch(
-    `${API_BASE_URL}/search?query=${query}&page=${pageNum}&page_size=${pageSize}`
+    `${API_BASE_URL}/search?title=${title}&author=${author}&publisher=${publisher}&subject=${subject}&keyword=${keyword}&page=${pageNum}&page_size=${pageSize}`
   );
   if (!res.ok) {
     throw new Error("Failed to fetch books");
