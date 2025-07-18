047d5d9c87c5b6642bcf32f4b6c4018c37becce4
“tmk-a” <tamaki.akita.jp@gmail.com>
2025-07-18 11:32:01 -0700

    fix: Modify SearchInput as a core component

--- frontend/src/core/components/input/SearchInput.tsx
+++ frontend/src/core/components/input/SearchInput.tsx
@@ -1,11 +1,34 @@
+import { Search } from "lucide-react";
+import "./SearchInput.scss";
+
 type Props = {
+  type: string;
   query: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   name: string;
+  placeholder?: string;
 };
 
-export const SearchInput = ({ query, onChange, name }: Props) => {
+const BASE_CLASS = "search-input";
+
+export const SearchInput = ({
+  type,
+  query,
+  onChange,
+  name,
+  placeholder,
+}: Props) => {
   return (
-    <input type="text" value={query} onChange={onChange} placeholder={name} />
+    <div className={`${BASE_CLASS}__wrapper`}>
+      <Search className={`${BASE_CLASS}__icon`} />
+      <input
+        type={type}
+        name={name}
+        value={query}
+        onChange={onChange}
+        placeholder={placeholder ? placeholder : "Enter a value..."}
+        className={`${BASE_CLASS}__input`}
+      />
+    </div>
   );
 };
